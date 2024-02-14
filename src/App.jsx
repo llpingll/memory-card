a73017a0a88b7c647f48f901e85a73b883942931
llpingll <94099281+llpingll@users.noreply.github.com>
2024-02-14 19:49:45 +0000

    Add basic file structure

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,16 @@
-import { useState } from 'react'
-import './App.css'
+import React from "react";
+import Header from "./components/Header";
+import Main from "./components/Main";
+import Footer from "./components/Footer";
 
 function App() {
-
   return (
     <>
-      
+      {/* <Header />
+      <Main />
+      <Footer /> */}
     </>
-  )
+  );
 }
 
-export default App
+export default App;
