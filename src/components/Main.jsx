f200c229d0300d6fa0d6f03cbc7076dc351f3f5f
llpingll <94099281+llpingll@users.noreply.github.com>
2024-02-20 20:58:18 +0000

    Complete Scoreboard component

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,9 +1,19 @@
 import React from "react";
+import Scoreboard from "./scoreboard/Scoreboard";
+import styled from "styled-components";
 
-// const Main = () => {
-//     return (
+const Main = () => {
+  return (
+    <MainContainer>
+      <Scoreboard></Scoreboard>
+      {/* <Cardsgrid></Cardsgrid> */}
+    </MainContainer>
+  );
+};
 
-//     );
-// }
+const MainContainer = styled.div`
+  display: flex;
+  justify-content: center;
+`;
 
-// export default Main;
+export default Main;
---
+++ src/components/scoreboard/Scoreboard.jsx
@@ -0,0 +1,49 @@
+import React from "react";
+import styled from "styled-components";
+
+const Scoreboard = ({ currentScore, bestScore }) => {
+  return (
+    <ScoreContainer>
+      <CurrentScore>Current Score: {currentScore}</CurrentScore>
+      <BestScore>Best Score: {bestScore}</BestScore>
+    </ScoreContainer>
+  );
+};
+
+const ScoreContainer = styled.div`
+  display: flex;
+  justify-content: center;
+  /* align-items: center; */
+  width: 69rem;
+  border: green solid 2px;
+
+  @media (max-width: 750px) {
+    flex-direction: column;
+    width: 250px;
+    gap: 3rem;
+  }
+`;
+
+const Score = styled.div`
+  padding: 0.5rem;
+  width: 35%;
+  font-size: 2rem;
+  border-radius: 20px;
+  font-weight: 700;
+  text-align: center;
+  box-shadow: rgba(0, 0, 0, 0.35) 0px 5px 15px;
+
+  @media (max-width: 750px) {
+    width: 100%;
+  }
+`;
+
+const CurrentScore = styled(Score)`
+  background-color: ${({ theme }) => theme.colors.red};
+`;
+
+const BestScore = styled(Score)`
+  background-color: ${({ theme }) => theme.colors.green};
+`;
+
+export default Scoreboard;
